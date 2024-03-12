75e26ba9fc37711dd2614f2493f2d119ac503109
donaath <131991017+donaath@users.noreply.github.com>
2024-03-12 03:23:12 +0300

    made Score a singleton class

--- src/Score.java
+++ src/Score.java
@@ -2,14 +2,22 @@ import java.awt.*;
 
 public class Score extends Rectangle{
 
-    static int GAME_WIDTH;
-    static int GAME_HEIGHT;
-    int player1;
-    int player2;
+    private static Score instance = null;
+    
+    private static int GAME_WIDTH;
+    private static int GAME_HEIGHT;
+    private int player1;
+    private int player2;
 
-    Score(int GAME_WIDTH, int GAME_HEIGHT){
+    private Score(int GAME_WIDTH, int GAME_HEIGHT) {
         Score.GAME_WIDTH = GAME_WIDTH;
         Score.GAME_HEIGHT = GAME_HEIGHT;
+    }
+    public static Score getInstance(int GAME_WIDTH, int GAME_HEIGHT) {
+        if (instance == null) {
+            instance = new Score(GAME_WIDTH, GAME_HEIGHT);
+        }
+        return instance;
     }
     public void draw(Graphics g) {
         g.setColor(Color.white);
@@ -19,5 +27,21 @@ public class Score extends Rectangle{
 
         g.drawString(String.valueOf(player1/10)+String.valueOf(player1%10), (GAME_WIDTH/2)-85, 50);
         g.drawString(String.valueOf(player2/10)+String.valueOf(player2%10), (GAME_WIDTH/2)+20, 50);
+    }
+    
+    public int getPlayer1() {
+        return player1;
+    }
+
+    public int getPlayer2() {
+        return player2;
+    }
+
+    public void incrementPlayer1() {
+        player1++;
+    }
+
+    public void incrementPlayer2() {
+        player2++;
     }
 }
