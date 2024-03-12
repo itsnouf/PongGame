bf4c6e4c47edf0edc069f9274e710b4c159d43a5
shadanyousef <120052739+shadanyousef@users.noreply.github.com>
2024-03-12 14:54:33 +0300

    Update Paddle.java
    Addition of the setColor method, Modification of the draw method

--- src/Paddle.java
+++ src/Paddle.java
@@ -6,10 +6,16 @@ public class Paddle extends Rectangle{
     int id;
     int yVelocity;
     int speed = 10;
+    private Color color; // Color of the paddle
 
     Paddle(int x, int y, int PADDLE_WIDTH, int PADDLE_HEIGHT, int id){
         super(x,y,PADDLE_WIDTH,PADDLE_HEIGHT);
         this.id=id;
+        this.color = (id == 1) ? Color.green : Color.yellow; // Default color for the paddle
+    }
+    
+    public void setColor(Color color) {
+        this.color = color;
     }
 
     public void keyPressed(KeyEvent e) {
@@ -42,11 +48,9 @@ public class Paddle extends Rectangle{
     public void move() {
         y = y + yVelocity;
     }
+    
     public void draw(Graphics g) {
-        if(id==1)
-            g.setColor(Color.green);
-        else
-            g.setColor(Color.yellow);
+        g.setColor(color);
         g.fillRect(x, y, width, height);
     }
 }
