7e483f073bcdaea705d0fb85866266deb95fca4e
donaath <131991017+donaath@users.noreply.github.com>
2024-05-04 20:06:04 +0300

    Update Adapter.java

--- src/Adapter.java
+++ src/Adapter.java
@@ -1,44 +1,47 @@
 import java.io.IOException;
-
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
-public class Adapter {
-  LoginSignup LogSignUp ;
+public class Adapter implements Authenticator {
+    private final LoginSignupProxy loginSignupProxy;
 
-public Adapter(LoginSignup LogSignUp){
-    this.LogSignUp = LogSignUp  ;
+    public Adapter(LoginSignupProxy loginSignupProxy) {
+        this.loginSignupProxy = loginSignupProxy;
+    }
 
-}
+    @Override
+    public boolean authenticate(String username, String password) throws IOException {
+        return loginSignupProxy.authenticate(username, password);
+    }
 
-public boolean authentication(String username, String password) {
-   return LogSignUp.authenticate(username, password);
-}
-public boolean usercreation(String username, String password) throws IOException{
-    return LogSignUp.createUser(username, password);
-   
-}
-public  void loadingData() throws IOException {
-   LogSignUp.loadUserData();
-}
+    @Override
+    public boolean createUser(String username, String password) throws IOException {
+        return loginSignupProxy.createUser(username, password);
+    }
 
+    public void loadUserData() throws IOException {
+        loginSignupProxy.loadUserData();
+    }
 
-public void savingData() throws IOException {
-    LogSignUp.saveUserData();
-}
+    public void saveUserData() throws IOException {
+        loginSignupProxy.saveUserData();
+    }
 
-public static void ShowGUI() {
-    LoginSignup.createAndShowGUI();
-}
-public  boolean Userauthentication(String username, String password) throws IOException{
-    return LogSignUp.authenticateUser(username, password);
-}
-public boolean createNewUser(String username, String password) throws IOException {
-    return LogSignUp.createNewUser(username, password);
-}
-public  void clearFields(JTextField usernameField, JPasswordField passwordField) {
-    LogSignUp.clearFields(usernameField , passwordField);
-}
+    public static void ShowGUI() {
+        LoginSignup.createAndShowGUI();
+    }
+
+    public boolean Userauthentication(String username, String password) throws IOException {
+        return loginSignupProxy.authenticate(username, password);
+    }
+
+    public boolean createNewUser(String username, String password) throws IOException {
+        return loginSignupProxy.createUser(username, password);
+    }
+
+//    public void clearFields(JTextField usernameField, JPasswordField passwordField) {
+//        loginSignupProxy.clearFields(usernameField, passwordField);
+//    }
 }
 
 
