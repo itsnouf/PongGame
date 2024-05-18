1d7714c06da99a0a19b96bef4cc35e57abe8239f
lama-khalil1 <145604063+lama-khalil1@users.noreply.github.com>
2024-05-18 14:20:29 +0300

    Apply State Design pattern to PongGame class

--- src/PongGame.java
+++ src/PongGame.java
@@ -1,17 +1,38 @@
 import java.awt.*;
-import java.io.IOException;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
+import java.io.IOException;
 
-public class PongGame {
-    public static void main(String[] args) throws IOException {
+interface GameState {
+    void doAction(PongGame game) throws IOException;
+}
+
+
+
+class InitState implements GameState {
+    @Override
+    public void doAction(PongGame game) throws IOException {
+        System.out.println("Game is in initialization state");
         @SuppressWarnings("unused")
         LoginSignupProxy loginSignupProxy = new LoginSignupProxy();
         Adapter adapter = new Adapter(loginSignupProxy);
         adapter.ShowGUI();
+        game.setState(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Init State";
+    }
+}
+
+class SetupState implements GameState {
+    @Override
+    public void doAction(PongGame game) {
+        System.out.println("Game is in setup state");
 
         // Ask for ball speed choice
-        int speedChoice = askForBallSpeed();
+        int speedChoice = PongGame.askForBallSpeed();
 
         // Create the game panel with default paddle colors
         GamePanel gamePanel = GamePanel.getInstance(Color.GREEN, Color.YELLOW);
@@ -24,9 +45,65 @@ public class PongGame {
         frame.add(gamePanel);
         frame.pack();
         frame.setVisible(true);
+
+        game.setState(this);
     }
 
-    private static int askForBallSpeed() {
+    @Override
+    public String toString() {
+        return "Setup State";
+    }
+}
+
+class PlayState implements GameState {
+    @Override
+    public void doAction(PongGame game) {
+        System.out.println("Game is in play state");
+        // Implementation for playing state can be added here
+        game.setState(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Play State";
+    }
+}
+
+public class PongGame {
+    private GameState state;
+
+    public PongGame() {
+        state = null;
+    }
+
+    public void setState(GameState state) {
+        this.state = state;
+    }
+
+    public GameState getState() {
+        return state;
+    }
+
+    public static void main(String[] args) throws IOException {
+        PongGame game = new PongGame();
+
+        // Initial state
+        InitState initState = new InitState();
+        initState.doAction(game);
+        System.out.println(game.getState().toString());
+
+        // Setup state
+        SetupState setupState = new SetupState();
+        setupState.doAction(game);
+        System.out.println(game.getState().toString());
+
+        // Transition to play state (for demonstration)
+        PlayState playState = new PlayState();
+        playState.doAction(game);
+        System.out.println(game.getState().toString());
+    }
+
+    public static int askForBallSpeed() {
         System.out.println("Enter the ball speed (1 for slow, 2 for medium, 3 for fast):");
         int speedChoice = 2; // Default to medium speed
         try (Scanner scanner = new Scanner(System.in)) {
@@ -39,5 +116,3 @@ public class PongGame {
         return speedChoice;
     }
 }
-
-
